8eb1ddcf4c96de9a2eebd104ffd9f78d08d94dc9
deveeking <70712107+deveeking@users.noreply.github.com>
2020-10-06 10:58:38 +0300

    [QA][AC] fix

--- src/test/java/Tests/TestRunersTest.java
+++ src/test/java/Tests/TestRunersTest.java
@@ -3,8 +3,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features =  "src/test/Scenario/",
-        glue = "src/main/java/Steps/"
+
+        tags = "@tags",
+        features = "src\\test\\java\\Scenario",
+        glue = "src\\main\\java\\Steps"
 )
 
 public class TestRunersTest extends AbstractTestNGCucumberTests {
